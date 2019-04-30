453954da995fbe6c58488e5e548cbf4864484966
William Bainton <baintonw@gmail.com>
2019-04-30 18:01:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,7 +15,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
+
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
   number_of_times.times do
@@ -23,7 +23,7 @@ def times_iterator(number_of_times)
 
   end
 end
-  
+
 end
 
 def while_iterator(number_of_times)
